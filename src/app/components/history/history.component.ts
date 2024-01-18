16ea226a8ead46b009c76548c89d850ff972bbe1
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-01-17 23:44:01 -0300

    list history

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  tasks: Task[] = []
+  constructor(private taskService: TaskService) {}
 
+  ngOnInit(): void {
+    this.taskService.list().subscribe(tasks => {
+      this.tasks = tasks
+    })
+  }
 }
